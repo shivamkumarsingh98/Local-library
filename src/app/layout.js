63c43d9a7aab6ddb154a90100c855f75c9fdd27a
shivamkumarsingh98 <shivamkumarsingh.chu98@gmail.com>
2025-10-12 03:25:37 +0530

    add header name or logo

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,8 +2,11 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
 export const metadata = {
-  title: "MyLanding",
-  description: "Discover top libraries and tools",
+  title: "Local Libraries",
+  description: "Library booking platform",
+  icons: {
+    icon: "/images/Final Logo.jpg",
+  },
 };
 export default function RootLayout({ children }) {
   return (
